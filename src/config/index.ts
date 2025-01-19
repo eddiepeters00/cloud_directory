5b56c24e1ed82304470483f1db7a25bcbf5e1822
eddiepeters00 <eddie.peters00@outlook.com>
2025-01-19 19:55:51 +0100

    Added database config and set up mongoDB

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,18 @@ dotenv.config();
 const NODE_HOSTNAME = process.env.NODE_HOSTNAME;
 const NODE_PORT = process.env.NODE_PORT;
 
+const MONGO_DB_URL = process.env.MONGODB_DB_URL;
+const MONGO_DB_NAME = process.env.MONGODB_DB_NAME;
+const MONGO_DB_COLLECTION = process.env.MONGODB_DB_COLLECTION;
+
+const DB_CONFIG = {
+  dbName: `${MONGO_DB_NAME}`,
+  dbUri: `${MONGO_DB_URL}/${MONGO_DB_NAME}`,
+  dbColl: `${MONGO_DB_COLLECTION}`,
+};
+
 export default Object.freeze({
   NODE_HOSTNAME,
   NODE_PORT,
+  DB_CONFIG,
 });
---
+++ src/data-access/index.ts
@@ -0,0 +1,16 @@
+import {
+  insertOneDocument,
+  updateDocument as makeUpdateDocument,
+  findDocuments as makeFindDocuments,
+} from "../libs/mongoDb";
+
+const insertDocument = ({ document, dbConfig }) =>
+  insertOneDocument({ document, ...dbConfig });
+
+const updateDocument = ({ query, values, dbConfig }) =>
+  makeUpdateDocument({ query, values, ...dbConfig });
+
+const findDocuments = ({ query, dbConfig }) =>
+  makeFindDocuments({ query, ...dbConfig });
+
+export { findDocuments, insertDocument, updateDocument };
